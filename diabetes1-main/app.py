60c9e3040b2b787766d96fd809a01981da2729d2
AishwaryaBCU <uaishwarya248@gmail.com>
2024-07-19 23:24:30 +0530

    Update app.py

--- diabetes1-main/app.py
+++ diabetes1-main/app.py
@@ -4,67 +4,59 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # Set page configuration
-st.set_page_config(page_title="Health Assistant",
-                   layout="wide",
-                   page_icon="🧑‍⚕️")
+st.set_page_config(
+    page_title="Health Assistant",
+    layout="wide",
+    page_icon="🧑‍⚕️"
+)
 
-# getting the working directory of the main.py
+# Getting the directory of the main.py
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
+# Define the absolute path to your model file
+model_file_path = os.path.join(working_dir, 'diabetes_model.sav')
 
+# Loading the saved model
+try:
+    with open(model_file_path, 'rb') as f:
+        diabetes_model = pickle.load(f)
+except FileNotFoundError:
+    st.error(f"Could not find the file {model_file_path}. Make sure the file exists.")
 
+# Displaying the app
+st.title('Diabetes Prediction using ML')
 
-# loading the saved models
+col1, col2, col3 = st.columns(3)
+with col1:
+    Pregnancies = st.text_input('Number of Pregnancies')
+with col2:
+    Glucose = st.text_input('Glucose Level')
+with col3:
+    BloodPressure = st.text_input('Blood Pressure value')
+with col1:
+    SkinThickness = st.text_input('Skin Thickness value')
+with col2:
+    Insulin = st.text_input('Insulin Level')
+with col3:
+    BMI = st.text_input('BMI value')
+with col1:
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+with col2:
+    Age = st.text_input('Age of the Person')
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
-
-
-
-# Sidebar for navigation
-with st.sidebar:
-    selected = option_menu('Machine Learning Diseases Prediction System',
-                           ['Diabetes Prediction'],
-                           menu_icon='hospital-fill',
-                           icons=['activity'],
-                           default_index=0)
-
-# Diabetes Prediction Page
-if selected == 'Diabetes Prediction':
-    st.title('Diabetes Prediction using ML')
-    
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
-    with col2:
-        Glucose = st.text_input('Glucose Level')
-    with col3:
-        BloodPressure = st.text_input('Blood Pressure value')
-    with col1:
-        SkinThickness = st.text_input('Skin Thickness value')
-    with col2:
-        Insulin = st.text_input('Insulin Level')
-    with col3:
-        BMI = st.text_input('BMI value')
-    with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    with col2:
-        Age = st.text_input('Age of the Person')
-
-    diab_diagnosis = ''
-    if st.button('Diabetes Test Result'):
-        try:
-            user_input = [float(Pregnancies), float(Glucose), float(BloodPressure), float(SkinThickness),
-                          float(Insulin), float(BMI), float(DiabetesPedigreeFunction), float(Age)]
-            diab_prediction = diabetes_model.predict([user_input])
-            if diab_prediction[0] == 1:
-                diab_diagnosis = 'The person is diabetic'
-            else:
-                diab_diagnosis = 'The person is not diabetic'
-            st.success(diab_diagnosis)
-        except ValueError:
-            st.error('Please enter valid numbers for all fields.')
-
-
+diab_diagnosis = ''
+if st.button('Diabetes Test Result'):
+    try:
+        user_input = [float(Pregnancies), float(Glucose), float(BloodPressure), float(SkinThickness),
+                      float(Insulin), float(BMI), float(DiabetesPedigreeFunction), float(Age)]
+        diab_prediction = diabetes_model.predict([user_input])
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'The person is diabetic'
+        else:
+            diab_diagnosis = 'The person is not diabetic'
+        st.success(diab_diagnosis)
+    except ValueError:
+        st.error('Please enter valid numbers for all fields.')
 
 # Running the app
 if __name__ == '__main__':
