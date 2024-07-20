39f9106759349dc65ae7ae99cb3832b75b7b012f
AishwaryaBCU <uaishwarya248@gmail.com>
2024-07-20 14:59:01 +0530

    Update config.py

--- Alzheimers/config.py
+++ Alzheimers/config.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 
-# Define paths
+# Define the base directory for assets
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ASSETS_DIR = os.path.join(BASE_DIR, 'assets', 'images')
 
@@ -9,6 +9,3 @@ ASSETS_DIR = os.path.join(BASE_DIR, 'assets', 'images')
 SIDE_BANNER = os.path.join(ASSETS_DIR, 'side_banner.webp')
 BG_IMAGE = os.path.join(ASSETS_DIR, 'bg.webp')
 DEFAULT_IMAGE = os.path.join(ASSETS_DIR, 'default.webp')
-
-# Load secrets
-NEWS_API_KEY = st.secrets.get("NEWS_API", None)  # Ensure you handle missing secrets properly
